5b06e740d74225ba13d700ba60625095459a70eb
Jimil Desai <jimildesai42@gmail.com>
2020-09-27 16:57:15 +0530

    REST APIs added

---
+++ product-api/handlers/product.go
@@ -0,0 +1,98 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"regexp"
+	"strconv"
+
+	"github.com/jimil749/go-microservices-practice/product-api/data"
+)
+
+// Products struc
+type Products struct {
+	l *log.Logger
+}
+
+// NewProducts creates a new products handler with the given logger.
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		// expect id in uri
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		grp := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(grp) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(grp[0]) != 2 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := grp[0][1]
+		id, err := strconv.Atoi(idString)
+
+		if err != nil {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		p.updateProduct(id, rw, r)
+	}
+
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	lp := data.GetProducts()
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal to json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+	data.AddProduct(prod)
+}
+
+func (p *Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product Not Found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Product Not Found", http.StatusNotFound)
+		return
+	}
+}
